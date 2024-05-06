2946156b5ed0772c24748eb1fdd9e1566478598d
LindaPermata <permatalinda531@gmail.com>
2024-05-06 15:20:00 +0700

    membuat int main

--- pointertiga.cpp
+++ pointertiga.cpp
@@ -25,8 +25,7 @@ class siswa
 
 void siswa::displayAll(orang &a)
 {
-    cout << id << endl
-         << a.nama;
+    cout << id << endl << a.nama;
 }
 void orang::setNama(string pNama)
 {
@@ -35,4 +34,15 @@ void orang::setNama(string pNama)
 void siswa::setId(int pId)
 {
     id = pId;
+}
+
+int main()
+{
+    orang joko;
+    joko.setNama("Joko Susilo");
+    siswa joko_siswa;
+    joko_siswa.setId(1);
+    joko_siswa.displayAll(joko);
+    return 0;
+    
 }
